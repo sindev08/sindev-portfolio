0b992657384136fea07322ba4b0dc6f80c755853
Singgih Budi Purnadi <57170834+sindev08@users.noreply.github.com>
2023-04-01 10:03:53 +0700

    setup vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
-// import { Navbar } from "components";
-// import { useEffect, useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 if (process.env.NODE_ENV !== "production") {
 	require("../assets/styles/globals.css");
 } else {
@@ -7,15 +6,12 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 function MyApp({ Component, pageProps }) {
-	// const [user, setUser] = useState({});
-	// useEffect(() => {
-	// 	const user = {
-	// 		name: "John Doe",
-	// 		avatar: "https://randomuser.me/api/portraits/med/men/75.jpg",
-	// 	};
-	// 	setUser(user);
-	// }, []);
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 }
 
 export default MyApp;
